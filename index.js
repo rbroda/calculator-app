4addf0763bcb3cffaec25214eecfb34a1f788fb3
Ryan Broda <brodaryan@gmail.com>
2025-11-04 19:51:16 -0500

    created all variables for buttons

---
+++ index.js
@@ -0,0 +1,34 @@
+const screen = document.getElementById("screen");
+const buttonZero = document.getElementById("button-zero");
+const buttonOne = document.getElementById("button-one");
+const buttonTwo = document.getElementById("button-two");
+const buttonThree = document.getElementById("button-three");
+const buttonFour = document.getElementById("button-four");
+const buttonFive = document.getElementById("button-five");
+const buttonSix = document.getElementById("button-six");
+const buttonSeven = document.getElementById("button-seven");
+const buttonEight = document.getElementById("button-eight");
+const buttonNine = document.getElementById("button-nine");
+const buttonAdd = document.getElementById("button-add");
+const buttonSubtract = document.getElementById("button-subtract");
+const buttonMultiply = document.getElementById("button-multiply");
+const buttonDivide = document.getElementById("button-divide");
+const buttonDecimal = document.getElementById("button-decimal");
+
+const add = function (int1, int2) {
+  return int1 + int2;
+};
+
+const subtract = function (int1, int2) {
+  return int1 - int2;
+};
+
+const multiply = function (int1, int2) {
+  return int1 * int2;
+};
+
+const divide = function (int1, int2) {
+  return int1 / int2;
+};
+
+const operate = function (op, int1, int2) {};
